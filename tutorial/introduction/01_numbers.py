0f01da3f30c2a0830e59c3099cba228c53a9a3f0
unknown <ikuko.otani.dev@gmail.com>
2026-02-22 07:13:08 +0900

    Add 01_numbers: numbers and basic operations

---
+++ tutorial/introduction/01_numbers.py
@@ -0,0 +1,15 @@
+# 3. A Non-Formal Intruduction to Python
+# 3.1.1 Numbers
+print(2+2)
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+
+print(7/3)      # float
+print(7 // 3)   # floor division
+print(7 % 3)    # modulo
+
+print(2 ** 10)  # exponentiation
+
+nums = [1, 2, 3, 4]
+n = len(nums)
+print("len(nums) = ", n)
