94d61bd46fc84c9533f9675917def15fb4aadfd5
ikuko-otani <ikuko.otani.dev@gmail.com>
2026-02-22 16:34:54 +0900

    Add more examples for arithmetic operations

--- tutorial/introduction/01_numbers.py
+++ tutorial/introduction/01_numbers.py
@@ -13,3 +13,11 @@ print(2 ** 10)  # exponentiation
 nums = [1, 2, 3, 4]
 n = len(nums)
 print("len(nums) = ", n)
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+price += price * tax
+print(price)  # assignment operator
+print(round(price, 2))
