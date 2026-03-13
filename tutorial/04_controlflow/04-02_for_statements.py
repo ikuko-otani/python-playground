f324a4a9f8c4fa7f6f8db59f012215753baafa43
ikuko-otani <ikuko.otani.dev@gmail.com>
2026-03-13 15:50:06 +0900

    Practice for-loops for sum and max calculations

--- tutorial/04_controlflow/04-02_for_statements.py
+++ tutorial/04_controlflow/04-02_for_statements.py
@@ -1,4 +1,4 @@
-words  = ["cat", "window", "defenestrate"]
+words = ["cat", "window", "defenestrate"]
 
 print("Loop 1: iterate over elsements")
 for w in words:
@@ -7,4 +7,25 @@ for w in words:
 print("Loop 2: iterate over indices")
 for i in range(len(words)):
   print(i, words[i], len(words[i]))
+
+numbers = [1, 3, 5, 2, 4]
+
+# Sum all numbers using a for-loop
+total = 0
+for x in numbers:
+  total += x
+print("Sum: ", total)
+
+# Find the maximum using indices
+max_value = numbers[0]
+for i in range(1, len(numbers)):
+  if numbers[i] > max_value:
+    max_value = numbers[i]
+print("Max :", max_value)
+
+max_value = numbers[0]
+for n in numbers:
+  if n > max_value:
+    max_value = n
+print("Max :", max_value)
 
