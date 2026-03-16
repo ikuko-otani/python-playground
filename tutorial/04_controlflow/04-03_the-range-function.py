afbdab60a8df4c9795c91553eca0d4bdc63fdebd
ikuko-otani <ikuko.otani.dev@gmail.com>
2026-03-17 04:36:33 +0900

    Practice index-based loops with range() for arrays

--- tutorial/04_controlflow/04-03_the-range-function.py
+++ tutorial/04_controlflow/04-03_the-range-function.py
@@ -4,13 +4,13 @@ for i in range(5):
 
 print("---")
 
-# range(start, end)
+# range(start, stop)
 for i in range(5, 10):
   print("range(5, 10):", i)
 
 print("---")
 
-# range(start, end, step)
+# range(start, stop, step)
 for i in range(0, 10, 3):
   print("range(0, 10, 3):", i)
 
@@ -19,3 +19,19 @@ print("---")
 # Negative step
 for i in range(-10, -100, -30):
   print("range(-10, -100, -30):", i)
+
+words = ['Mary', 'had', 'a', 'little', 'lamb']
+
+# Iterate over indices using range(len(...))
+for i in range(len(words)):
+  print("index:", i, "word:", words[i])
+
+nums = [1, 3, 5, 7]
+
+# Compare neighbors using indices
+for i in range(len(nums) - 1):
+  print("pair:", nums[i], nums[i + 1])
+
+# Iterate backwords over indices
+for i in range(len(nums) - 1, -1, -1):
+  print("reverse index:", i, "value:", nums[i])
